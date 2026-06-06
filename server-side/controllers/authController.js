5cfb3c6d7aac35e2e2d95e40083f22301f6958c3
Mohamed Essam <mohamedessam9523@gmail.com>
2026-06-06 03:51:16 +0300

    creating generateToken util and test generating the first token

---
+++ server-side/controllers/authController.js
@@ -0,0 +1,9 @@
+export const registerUser = async (req, res) => {};
+
+export const loginUser = async (req, res) => {};
+
+export const getProfile = async (req, res) => {
+  res.json({
+    message: "profile route works",
+  });
+};
